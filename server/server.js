86290e4fc404ad0b8d2b671e0ca4f1f865c36c97
Ayush <ayush.raj.sd@gmail.com>
2024-07-08 13:18:40 +0530

    updated rate limit

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ connectDB();
 // Rate limiter middleware
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
+  max: 1000, // Limit each IP to 100 requests per windowMs
   message: "Too many requests from this IP, please try again after 15 minutes",
 });
 
