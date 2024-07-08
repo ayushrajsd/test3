479a719716ff4d3a5c9e87019b299253a53d166b
Ayush <ayush.raj.sd@gmail.com>
2024-07-08 13:06:49 +0530

    updated axios base url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://test3-99k4.onrender.com/",
   headers: {
     "Content-Type": "application/json",
     authorization: `Bearer ${localStorage.getItem("token")}`,
